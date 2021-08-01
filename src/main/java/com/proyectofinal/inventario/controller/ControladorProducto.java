44af328002daba61b894a7da32642a079ff20feb
santiago cortes <jeissoncortes02@outlook.com>
2021-08-01 18:20:09 -0500

    post esta hecho y el listar

---
+++ src/main/java/com/proyectofinal/inventario/controller/ControladorProducto.java
@@ -0,0 +1,31 @@
+package com.proyectofinal.inventario.controller;
+
+import com.proyectofinal.inventario.entity.Producto;
+import com.proyectofinal.inventario.service.ServicioProducto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ControladorProducto {
+
+    private final ServicioProducto servicioProducto;
+
+    public ControladorProducto(ServicioProducto servicioProducto) {
+        this.servicioProducto = servicioProducto;
+    }
+
+    @PostMapping
+    public ResponseEntity<Producto> guardarProducto(@Valid @RequestBody Producto producto ){
+        return new ResponseEntity<>(servicioProducto.guardarProducto(producto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Producto>> listarProductos(){
+        return new ResponseEntity<>(servicioProducto.listarProductos(),HttpStatus.OK);
+    }
+}
