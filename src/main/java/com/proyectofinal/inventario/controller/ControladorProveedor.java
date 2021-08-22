3ee4bccc379ffe328d20173b6d56296a747f1ed6
santiago cortes <jeissoncortes02@outlook.com>
2021-08-22 17:15:40 -0500

    controlador parte hecho

---
+++ src/main/java/com/proyectofinal/inventario/controller/ControladorProveedor.java
@@ -0,0 +1,44 @@
+package com.proyectofinal.inventario.controller;
+
+import com.proyectofinal.inventario.dto.RespuestaDTO;
+import com.proyectofinal.inventario.entity.Proveedor;
+import com.proyectofinal.inventario.service.ServicioProveedor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/proveedores")
+public class ControladorProveedor {
+
+    private final ServicioProveedor servicioProveedor;
+
+    public ControladorProveedor(ServicioProveedor servicioProveedor) {
+        this.servicioProveedor = servicioProveedor;
+    }
+
+    @PostMapping
+    public RespuestaDTO<Proveedor> guardarProveedor(@RequestBody @Valid Proveedor proveedor){
+        return new RespuestaDTO<Proveedor>(
+                false,
+                HttpStatus.CREATED.value(),
+                "Se guardo con exito el proveedor",
+                servicioProveedor.guardarProveedor(proveedor),
+                String.valueOf(System.currentTimeMillis())
+        );
+    }
+
+    @GetMapping
+    public RespuestaDTO listarProveedores(){
+        return new RespuestaDTO(
+                false,
+                HttpStatus.OK.value(),
+                "Se listaron con exito los proveedores",
+                servicioProveedor.listarProveedor(),
+                String.valueOf(System.currentTimeMillis())
+        );
+    }
+
+
+}
