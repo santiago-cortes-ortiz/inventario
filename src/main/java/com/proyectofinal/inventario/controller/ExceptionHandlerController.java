1e56f6f7493eb2f079d9395eb1e82072c8f13ef7
santiago cortes <jeissoncortes02@outlook.com>
2021-09-21 17:36:23 -0500

    mas excepciones

--- src/main/java/com/proyectofinal/inventario/controller/ExceptionHandlerController.java
+++ src/main/java/com/proyectofinal/inventario/controller/ExceptionHandlerController.java
@@ -1,6 +1,8 @@
 package com.proyectofinal.inventario.controller;
 
 import com.proyectofinal.inventario.dto.RespuestaDTO;
+import com.proyectofinal.inventario.exception.ProveedorException;
+import com.proyectofinal.inventario.exception.UsuarioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +12,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionHandlerController {
     @ExceptionHandler(Exception.class)
     public RespuestaDTO<Object> exception(Exception e) {
+        return new RespuestaDTO(true, HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage(),String.valueOf(System.currentTimeMillis()));
+    }
+
+    @ExceptionHandler(ProveedorException.class)
+    public RespuestaDTO<Object> exception(ProveedorException e) {
+        return new RespuestaDTO(true, HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage(),String.valueOf(System.currentTimeMillis()));
+    }
+
+    @ExceptionHandler(UsuarioException.class)
+    public RespuestaDTO<Object> exception(UsuarioException e) {
         return new RespuestaDTO(true, HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage(),String.valueOf(System.currentTimeMillis()));
     }
 }
