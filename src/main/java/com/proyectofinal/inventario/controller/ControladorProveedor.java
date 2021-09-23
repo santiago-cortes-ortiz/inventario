e8ae7a5f118880e89448af3ea56b01d78f9503ee
santiago cortes <jeissoncortes02@outlook.com>
2021-09-22 20:05:30 -0500

    agregando documentacion de swagger al controlador

--- src/main/java/com/proyectofinal/inventario/controller/ControladorProveedor.java
+++ src/main/java/com/proyectofinal/inventario/controller/ControladorProveedor.java
@@ -3,6 +3,10 @@ package com.proyectofinal.inventario.controller;
 import com.proyectofinal.inventario.dto.RespuestaDTO;
 import com.proyectofinal.inventario.entity.Proveedor;
 import com.proyectofinal.inventario.service.ServicioProveedor;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +23,16 @@ public class ControladorProveedor {
     }
 
     @PostMapping
-    public RespuestaDTO<Proveedor> guardarProveedor(@RequestBody @Valid Proveedor proveedor){
+    @ApiOperation("Guarda un proveedor")
+    @ApiResponses(
+            {
+                    @ApiResponse(code = 201,message = "Guarda un proveedor con exito"),
+                    @ApiResponse(code = 500,message = "Ocurrio un error en el servidor")
+            }
+    )
+    public RespuestaDTO<Proveedor> guardarProveedor(
+            @ApiParam(value = "El cuerpo con el cual se guardara el proveedor", required = true) @RequestBody @Valid Proveedor proveedor
+    ){
         return new RespuestaDTO<Proveedor>(
                 false,
                 HttpStatus.CREATED.value(),
@@ -30,6 +43,8 @@ public class ControladorProveedor {
     }
 
     @GetMapping
+    @ApiOperation("Devuelve todos los proveedores")
+    @ApiResponse(code = 200,message = "OK")
     public RespuestaDTO listarProveedores(){
         return new RespuestaDTO(
                 false,
@@ -41,7 +56,14 @@ public class ControladorProveedor {
     }
 
     @PutMapping
-    public RespuestaDTO<Proveedor> modificarProveedor(@RequestBody @Valid Proveedor proveedor){
+    @ApiOperation("Actualiza un proveedor")
+    @ApiResponses(
+            {
+                    @ApiResponse(code = 204,message = "Se actualiza un proveedor con exito"),
+                    @ApiResponse(code = 500,message = "Ocurrio un error en el servidor")
+            }
+    )
+    public RespuestaDTO<Proveedor> modificarProveedor(@ApiParam(value = "El cuerpo con el cual se actualizar el proveedor", required = true)@RequestBody @Valid Proveedor proveedor){
         return new RespuestaDTO<>(
                 false,
                 HttpStatus.NO_CONTENT.value(),
